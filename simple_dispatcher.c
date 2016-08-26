31fdd61e9adcb4e206da51e3f7fd8bb7301c95ce
Stefan Klauck <Stefan.Klauck@amida.eaalab.hpi.uni-potsdam.de>
2016-08-26 17:46:29 +0200

    Added strnstr function, missing on some linux systems

--- simple_dispatcher.c
+++ simple_dispatcher.c
@@ -10,6 +10,21 @@
 #include "dbg.h"
 
 
+char *strnstr(const char *s1, const char *s2, size_t len)
+{
+	size_t l2;
+
+	l2 = strlen(s2);
+	if (!l2)
+		return (char *)s1;
+	while (len >= l2) {
+		len--;
+		if (!memcmp(s1, s2, l2))
+			return (char *)s1;
+		s1++;
+	}
+	return NULL;
+}
 
 int get_session_from_json(char *str, size_t str_len) {
     debug("get_session_from_json");
